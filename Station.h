d693f68dc106b683b1504c22bc9629230d8c2f25
KlaraMeyer0 <u20428082@tuks.co.za>
2021-11-16 23:56:41 +0200

    Station Header

--- Station.h
+++ Station.h
@@ -3,12 +3,17 @@
 
 using namespace std;
 
+#include "Cargo.h"
+
 class Station {
 private:
-    
+    Station* next;
 public:
 	Station();
 	~Station();
+    void add(Station* stat);
+    virtual void receiveCargo(Cargo* c);
+    virtual void receiveCommunication(string com);
 };
 
 #endif
