d94d5db28cdc31c9d8347bb83190513bc1dc0ce4
RRduToit <u20450533@Tuks.co.za>
2021-11-16 15:42:42 +0200

    Update RocketshipBay.h

--- RocketshipBay.h
+++ RocketshipBay.h
@@ -7,6 +7,9 @@ class RocketshipBay {
 public:
 	RocketshipBay();
     ~RocketshipBay();
+    virtual void buildRocket() = 0;
+    virtual void buildBody() = 0;
+    virtual void buildCargo() = 0;
 };
 
 #endif
