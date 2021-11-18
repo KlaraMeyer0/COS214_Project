aaa67f19437078ae38c337b272067bd7a75f7f91
Xander-FP <xdilo.coetzer@gmail.com>
2021-11-18 21:44:23 +0200

    only for Builder

--- RocketshipEngineer.h
+++ RocketshipEngineer.h
@@ -1,25 +1,17 @@
 #ifndef ROCKETSHIPENGINEER_H
 #define ROCKETSHIPENGINEER_H
 
-#include "Work.h"
 #include "RocketShipBay.h"
-#include "LaunchCaretaker.h"
 
-class RocketshipEngineer {
-private:
-	RocketshipBay* rb;
-	Work** command;
-	LaunchCaretaker** LaunchCaretakers;
+class RocketshipEngineer
+{
 public:
 	RocketshipEngineer();
 	~RocketshipEngineer();
-	void construct();
-	bool makeDragon();
-	bool makeCrewDragon();
-	bool makeStarlink();
-	bool createBackup();
-	bool retrieveBackup();
-	bool startTestLaunch();
+	void construct(int bay);
+
+private:
+	RocketshipBay **rb;
 };
 
 #endif
