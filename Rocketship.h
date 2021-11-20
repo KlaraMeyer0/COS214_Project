f3539fd4b49203b6434da831222cf07c6d085cb1
Xander-FP <xdilo.coetzer@gmail.com>
2021-11-20 15:02:40 +0200

    fixed countdown

--- Rocketship.h
+++ Rocketship.h
@@ -3,20 +3,22 @@
 
 using namespace std;
 #include <iostream>
+#include <unistd.h>
 #include "Station.h"
+
 
 class Rocketship
 {
 public:
-	Rocketship(string n, char);
+	Rocketship(string n, char);//initialises 
 	~Rocketship();
-	virtual void Launch() = 0;
+	virtual void Launch() = 0;//launches the rocket into space and attaches it to the space station
 	virtual Rocketship *clone() = 0;
-	string getName();
-	void countdown();
-	char getType();
-	void attachToStation(Station *);
-	Station *getStation();
+	string getName();//return name of the rocketship
+	void countdown();//countdown before launch
+	char getType();//returns the type of the rocket(DragonRocketship, CrewDragonRocketship or StarlinkCollection)
+	void attachToStation(Station *); //attaches the specefic rocketship to a base or space station
+	Station *getStation(); //returns the station that the rocketship is currently attached to.
 
 private:
 	string name;
