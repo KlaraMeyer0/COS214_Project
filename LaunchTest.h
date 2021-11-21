7abe5ccbaf1393d80e843b17adf27e444f75dd14
u20437863-James-Butler <u20437863@tuks.co.za>
2021-11-21 22:41:25 +0200

    Some DOXYGEN comments

--- LaunchTest.h
+++ LaunchTest.h
@@ -1,27 +1,66 @@
+/**
+ * @file LaunchTest.h
+ * @author James Butler
+ * 
+ * @brief 
+ * 
+ * Design Pattern: Decorator
+ * Participant: Decorator
+ */
 #ifndef LAUNCHTEST_H
 #define LAUNCHTEST_H
-
-using namespace std;
 
 #include "Launch.h"
 #include <iostream>
 #include <sstream>
 
+using namespace std;
+
 class LaunchTest : public Launch {
 public:
+	/**
+	 * @brief Construct a new Launch Test object
+	 * 
+	 * @param la Sets the Component to decorate
+	 */
 	LaunchTest(Launch* la){
 		l = la;
 		t = false;
 	};
+
+	/**
+	 * @brief Destroy the Launch Test object
+	 * 
+	 */
 	virtual ~LaunchTest(){
 		delete l;
 	};
+
+	/**
+	 * @brief Implements the concrete decorators' functions
+	 * 
+	 */
     virtual void output() = 0;
+
+	/**
+	 * @brief Calls the output() function and then the component's startLaunch() function
+	 * 
+	 */
 	virtual void startLaunch() = 0;
+
+	/**
+	 * @brief Get the Ship object
+	 * 
+	 * @return Rocketship* 
+	 */
 	virtual Rocketship* getShip(){
 		return l->getShip();
 	};
 protected:
+	/**
+	 * @brief The decorated component
+	 * 
+	 */
 	Launch* l;
 };
 
