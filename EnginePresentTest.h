dd95dd8b526f7400d014c47f655723d3cceeacce
u20437863-James-Butler <u20437863@tuks.co.za>
2021-11-21 23:31:08 +0200

    Doxygen

--- EnginePresentTest.h
+++ EnginePresentTest.h
@@ -1,15 +1,43 @@
+/**
+ * @file EnginePresentTest.h
+ * @author James Butler
+ * @brief 
+ * 
+ * Design Pattern: Decorator
+ * Participant: Concrete Decorator
+ */
 #ifndef ENGINEPRESENTTEST_H
 #define ENGINEPRESENTTEST_H
 
-using namespace std;
-
 #include "LaunchTest.h"
+
+using namespace std;
 
 class EnginePresentTest : public LaunchTest{
 public:
-	EnginePresentTest(Launch*);
+	/**
+	 * @brief Construct a new Engine Present Test object
+	 * 
+	 * @param la The Component to decorate
+	 */
+	EnginePresentTest(Launch* la);
+
+	/**
+	 * @brief Destroy the Engine Present Test object
+	 * 
+	 */
 	virtual ~EnginePresentTest();
+
+	/**
+	 * @brief Tests if the correct number of engines are attatched
+	 * 
+	 */
     void output();
+
+	/**
+	 * @brief Calls the output() function and the component's startLaunch() function
+	 * 
+	 */
 	virtual void startLaunch();
 };
 
