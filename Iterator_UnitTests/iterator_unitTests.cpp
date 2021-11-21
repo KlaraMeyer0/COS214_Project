39dd95de2bc24ec455d97f19b71506f7d5ab514c
KlaraMeyer0 <u20428082@tuks.co.za>
2021-11-21 18:17:02 +0200

    Iterator Unit Test

---
+++ Iterator_UnitTests/iterator_unitTests.cpp
@@ -0,0 +1,32 @@
+#include <limits.h>
+#include <stdexcept>
+#include <iostream>
+
+using namespace std;
+
+//#include "gtest/gtest.h"
+#include "StarlinkCollection.h"
+
+namespace
+{
+    // Tests state
+    void runIteratorTests() {
+        cout << "Creating a StarlinkCollection for testing." << endl;
+        StarlinkCollection* testCollection = new StarlinkCollection("TestCollection", nullptr, nullptr);
+        StarlinkSatellite* testSatellite1 = new StarlinkSatellite("Test location 1", nullptr);
+        StarlinkSatellite* testSatellite2 = new StarlinkSatellite("Test location 1", nullptr);
+        StarlinkSatellite* testSatellite3 = new StarlinkSatellite("Test location 1", nullptr);
+
+        testCollection->insert(testSatellite1);
+        testCollection->insert(testSatellite2);
+        testCollection->insert(testSatellite2);
+
+        int idx = 0;
+        SatelliteIterator* i;
+        for (i = testCollection->begin(); !(testCollection->end()); ++i)
+        {
+            cout << "Satellite " << idx << " communicates with: " << i->currentSatellite()->getCommunicatesWith() << endl;
+            idx++;
+        }
+    }
+}
