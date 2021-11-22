8db171cfac1f3161c33d9991aec18d6406a5d9cc
u20437863-James-Butler <u20437863@tuks.co.za>
2021-11-22 13:33:38 +0200

    Doc repairs

--- Backup.h
+++ Backup.h
@@ -3,8 +3,8 @@
  * @author James Butler
  * @brief 
  * 
- * Design Pattern: Command
- * Participant: Concrete Command
+ * Design Pattern: Memento, Command
+ * Participant: Client, Concrete Command
  */
 #ifndef BACKUP_H
 #define BACKUP_H
