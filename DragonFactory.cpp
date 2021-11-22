b31d31406d70adc5ac3e495eba7bab6f1569dc17
Xander-FP <xdilo.coetzer@gmail.com>
2021-11-22 15:18:22 +0200

    added output

--- DragonFactory.cpp
+++ DragonFactory.cpp
@@ -1,10 +1,12 @@
 #include "DragonFactory.h"
 #include "DragonSpacecraft.h"
+#include <iostream>
 
 DragonFactory::DragonFactory() {}
 DragonFactory::~DragonFactory() {}
 
 Spacecraft *DragonFactory::createRocket()
 {
+    cout << "Dragon spacecraft successfully create" << endl;
     return new DragonSpacecraft(50);
 }
