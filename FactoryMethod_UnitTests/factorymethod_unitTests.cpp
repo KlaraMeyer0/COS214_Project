23e1d01066ef2242bd8a752f429cc278fa5bf766
Xander-FP <xdilo.coetzer@gmail.com>
2021-11-22 13:44:31 +0200

    passively AHHH

--- FactoryMethod_UnitTests/factorymethod_unitTests.cpp
+++ FactoryMethod_UnitTests/factorymethod_unitTests.cpp
@@ -1,8 +1,48 @@
 #include <limits.h>
 #include <stdexcept>
+// #include "CrewDragonFactory.h"
+// #include "DragonFactory.h"
+// #include "DragonSpacecraft.h"
+// #include "CrewDragonSpacecraft.h"
 #include <gtest/gtest.h>
 
 namespace
 {
-    // Tests builder
+    // Test that all factories create successfully
+    // TEST()
+    // {
+
+    // }
+
+    // // Test CargoFactory Hierarchy
+    // TEST()
+    // {
+
+    // }
+    // TEST()
+    // {
+        
+    // }
+
+    // // Test RocketFactory Hierarchy
+    // TEST()
+    // {
+
+    // }
+    // TEST()
+    // {
+        
+    // }
+
+    // Test SpacecraftFactory Hierarchy
+    // TEST(DragonFactoryCreationTest, CreationTest)
+    // {
+    //     SpacecraftFactory* factory = new DragonFactory();
+    //     EXPECT_EQ(factory->startFactory(), new DragonSpacecraft(50));
+    // }
+    // TEST(CrewDragonFactoryCreationTest, CreationTest)
+    // {
+    //     SpacecraftFactory* factory = new CrewDragonFactory();
+    //     EXPECT_EQ(factory->startFactory(),new CrewDragonSpacecraft(75));
+    // }
 }
