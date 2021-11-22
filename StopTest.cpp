fb29169a5dd5573377a7e372664dd630130354dd
u20437863-James-Butler <u20437863@tuks.co.za>
2021-11-22 17:05:49 +0200

    Fixed errors

--- StopTest.cpp
+++ StopTest.cpp
@@ -6,14 +6,17 @@ void StopTest::output(){
     bool b = true;
     char c;
     do{
+        cin.clear();
+        cin.ignore(15,'\n');
         cout<<"The launch is suspended. Input c to continue or a to abort."<<endl;
         cin>>c;
         if (c == 'c'){
             flag = true;
             b = false;
-        } if (c == 'a')
+        } else if (c == 'a')
             b = false;
         else cout<<"Please input only c or a!"<<endl;
+        cin.clear();
         cin.ignore(15, '\n');
     } while (b);
 }
