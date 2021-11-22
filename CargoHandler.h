c5bfaaa44a06a36a0ac2861e23b468009f13d287
KlaraMeyer0 <u20428082@tuks.co.za>
2021-11-22 11:43:31 +0200

    updates

--- CargoHandler.h
+++ CargoHandler.h
@@ -17,11 +17,35 @@
 class CargoHandler
 {
     public:
+        /**
+         * Constructor for CargoHandler
+         */
         CargoHandler();
+
+        /**
+         * Destructor for CargoHandler
+         */
         ~CargoHandler();
+
+        /**
+         * Adds a concrete CargoHandler
+         *
+         * @param n Concrete CargoHandler
+         */
         void add(CargoHandler* n);
+
+        /**
+         * Calls the handleCargo implementations of the concrete CargoHandler classes in order for the
+         * cargo to be handled in the correct way
+         *
+         * @param c Cargo to be handled
+         * @param s Station to which cargo is sent
+         */
         virtual void handleCargo(Cargo* c, Station* s);
     private:
+        /**
+         * Stores the next concrete CargoHandler
+         */
         CargoHandler* next;
 };
 
