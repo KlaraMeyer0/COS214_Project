17ef37a2d7397a336265adb4b9956bd7881e7e40
KlaraMeyer0 <u20428082@tuks.co.za>
2021-11-23 01:13:15 +0200

    updates

--- SpaceStation.cpp
+++ SpaceStation.cpp
@@ -7,7 +7,8 @@ using namespace std;
 SpaceStation::SpaceStation() : Station()
 {
     setName("Space-RXKJL-68348");
-
+    //handler = new CargoHandler();
+    //handler->add(new HumanHandler(true));
     handler = new HumanHandler();
     handler->add(new EquipmentHandler());
 
@@ -16,7 +17,7 @@ SpaceStation::SpaceStation() : Station()
 
 SpaceStation::~SpaceStation()
 {
-
+    delete handler;
 }
 
 void SpaceStation::receiveCargo(Cargo *c)
@@ -31,33 +32,40 @@ void SpaceStation::receiveCommunication(string s)
 
 void SpaceStation::printEquipment()
 {
-    for (int i = 0; i < equipment.size(); i++)
-        cout << "Index: " << i << " Type: " << equipment.at(i).first->getName() << " Number: " << equipment.at(i).second << endl;
+    if (equipment.empty())
+        cout << "There is no equipment at the station." << endl;
+    else
+        for (int i = 0; i < equipment.size(); i++)
+            cout << "Index: " << (i + 1) << " Type: " << equipment.at(i).first->getName() << " Number: "
+                 << equipment.at(i).second << endl;
 }
 
 void SpaceStation::printHumans()
 {
-    for (int i = 0; i < humans.size(); i++)
-        cout << "index: " << i << " Name: " << humans.at(i) << endl;
+    if (humans.empty())
+        cout << "There are no humans at the station." << endl;
+    else
+        for (int i = 0; i < humans.size(); i++)
+            cout << "index: " << (i + 1) << " Name: " << humans.at(i)->getName() << endl;
 }
 
-pair<Cargo *, int> *SpaceStation::loadEquipment(int idx, int num)
+pair<Cargo *, int> SpaceStation::loadEquipment(int idx, int num)
 {
-    if (idx > equipment.size() - 1)
-        return nullptr;
+    pair<Cargo *, int> p;
+    p = make_pair(equipment.at(idx).first, p.second = num);
 
-    pair<Cargo*, int> * p;
-    p->first = equipment.at(idx).first;
-    p->second = num;
+    if (idx > equipment.size() - 1)
+    {
+        p.second = -1;
+        return p;
+    }
 
     if (equipment.at(idx).second > num)
     {
         equipment.at(idx).second -= num;
-    }
-    else
-    if (equipment.at(idx).second <= num)
+    } else if (equipment.at(idx).second <= num)
     {
-        p->second = equipment.at(idx).second;
+        p.second = equipment.at(idx).second;
         equipment.erase(equipment.begin() + idx);
     }
 
@@ -88,10 +96,6 @@ SpaceStation *SpaceStation::clone()
 
     for (int i = 0; i < humans.size(); i++)
         temp->humans.push_back(humans.at(i));
-
-    
-    temp->setName(this->getName());
-    temp->setSatStatus(this->getSatStatus());
 
     return temp;
 }
