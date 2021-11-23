78264fcbb009470ce0fb4f5580bdd915e51b6901
u20437863-James-Butler <u20437863@tuks.co.za>
2021-11-23 06:47:18 +0200

    Update StageEngine.cpp

--- StageEngine.cpp
+++ StageEngine.cpp
@@ -5,14 +5,14 @@ StageEngine::StageEngine() {};
 StageEngine::~StageEngine() {};
 
 bool StageEngine::testFire(){
-    cout<<"Powers up the engines for a static fire test..."<<endl;
+    cout<<"Powering up the engines for a static fire test..."<<endl;
     int r = 1+(rand() % 10);
     if (r == 1){
         cout<<"Some of the engines do not fire!"<<endl;
         return false;
     } else {
-        cout<<"All the engines fire at the currect time"<<endl;
-        cout<<"Powers down the engines after a successful test."<<endl;
+        cout<<"All the engines fire at the correct time"<<endl;
+        cout<<"Powering down the engines after a successful test."<<endl;
         return true;
     }
 }
